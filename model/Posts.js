943a804f0848805a599f52af53d4663bb7bca84a
salah <salah.shihaibi0@gmail.com>
2021-09-28 14:18:31 +0100

    Enable posting from dashboard and home

---
+++ model/Posts.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const postSchema = mongoose.Schema({
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  image: {
+    type: String,
+  }, 
+  status: {
+    type: Boolean,
+    require: true,
+  }, 
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'users',
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+
+});
+
+module.exports = mongoose.model("Posts", postSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
-
 const express = require('express');
 const router = express.Router();
 const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
+const Post = require('../model/Posts')
+
 
 /* GET home page. */
 router.get('/', forwardAuthenticated, (req, res) => {
@@ -14,5 +15,31 @@ router.get('/dashboard', ensureAuthenticated, (req, res) =>
     user: req.user
   })
 );
+
+// Get posts for home page
+router.get('/home', ensureAuthenticated, (req, res) => {
+  res.render('home', {
+    user: req.user
+  })
+});
+
+// add a post and redirect to home or posts
+router.post('/post', ensureAuthenticated, async (req, res) => {
+  try {
+    if(req.user){
+      if(req.body.description){
+        req.body.user = req.user.id
+        await Post.create(req.body)
+        res.redirect(req.body.URL)// we'll change this late
+      }else{
+        req.flash("error_msg", "Please provide a description");
+        res.redirect(req.body.URL)// we'll change this late
+      }  
+    }
+  } catch (err) {
+    console.error(err)
+  }
+})
+
 
 module.exports = router;
